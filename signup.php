99e32d0de3629197c37c9cd3f31f0d0018ac05e9
john wachira <john wachira>
2016-05-13 15:21:17 +0300

    this form creates news users from te database

---
+++ signup.php
@@ -0,0 +1,60 @@
+<?php 
+include "includes/session.php";
+include "includes/db.php";
+
+if (isset($_POST['submit'])){
+    //form credentials have been submitted
+    //get credentials
+    global $connection;
+    
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    
+    if (empty($username) || empty($password)){
+        die ("some registration fields were not filled in." . mysqli_error());
+    }
+    
+    $username = mysqli_real_escape_string($connection, $username );
+    $username = strtolower($username);
+    $password = mysqli_real_escape_string($connection, $password );
+    
+    //get credentials from database
+    $query = "INSERT INTO users (username, password) VALUE ('{$username}', '{$password}')";
+    $result = mysqli_query($connection, $query);
+ 
+    if (!$result){
+        die ("Could not insert values to the database" . mysqli_error());
+    } else {
+        //change the url to match the location of this file on your local server
+        $url = "http://localhost/digital_nomad/login.php";
+        redirect_to($url);
+    }
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sign Up</title>
+    <link rel="stylesheet" href="css/login.css">
+
+</head>
+<body>
+
+<div class="login">
+  <div class="login-triangle"></div>
+  
+  <h2 class="login-header">Sign Up</h2>
+
+  <form class="login-container" action="signup.php" method="post">
+    <p><input type="text" placeholder="Username" name="username"></p>
+    <p><input type="password" placeholder="Password" name="password"></p>
+    <p><input type="submit" name="submit" value="Sign Up"></p>
+  </form>
+</div>
+
+</body>
+</html>
