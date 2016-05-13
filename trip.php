9e42b16c3d6a75c3f2fd6969fc98f936f9a67d20
Kevin Murani <muranikun@gmail.com>
2016-05-13 13:01:52 +0300

    Added standard js add css libraries: jquery, bootstrap and handlebars

--- trip.php
+++ trip.php
@@ -6,9 +6,12 @@
   <head>
     <meta charset="utf-8">
     <title><?php echo $title ?></title>
-    
+    <?php include 'includes/stylesheets.php' ?>
   </head>
   <body>
 
+
+    <?php include 'includes/javascript.php' ?>
+    <script type="text/javascript"></script>
   </body>
 </html>
