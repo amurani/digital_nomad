1b3ec3bfd899a47fcc2e6635f64447e661f51859
john wachira <john wachira>
2016-05-13 16:55:46 +0300

    transfered the base url function to session.php

--- includes/globals.php
+++ includes/globals.php
@@ -2,13 +2,5 @@
 
   $title = "Digitial Nomads";
 
-  function get_base_url() {
-    $base_url = "http://localhost/digital_nomad/";
-    if (gethostname() == "Kevins-MacBook-Pro.local") {
-      $base_url = "http://localhost:8000/digital_nomad/";
-    }
-
-    return $base_url;
-  }
 
 ?>
