e61e4a6ee573a0c28abb97b0761358ce729370a7
Kevin Murani <muranikun@gmail.com>
2016-05-13 12:53:22 +0300

    Initial commit - Added a trip page to view the trip details and show books and places of interest. Added a global includes folder

---
+++ trip.php
@@ -0,0 +1,14 @@
+<?php
+  include 'includes/globals.php'
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title><?php echo $title ?></title>
+    
+  </head>
+  <body>
+
+  </body>
+</html>
