11506c77cddba855f1566ca42d8f86b397c69268
john wachira <john wachira>
2016-05-14 11:13:22 +0300

    added user selection insertion to db functionality

--- trip.php
+++ trip.php
@@ -6,8 +6,45 @@
 confirm_logged_in();
 ?>
  <?php
-  include 'includes/globals.php'
+  include 'includes/db.php';
+  include 'includes/globals.php';
 ?>
+
+<?php
+//get selected hotel and room from user
+global $connection;
+if (isset($_GET['user_id']) && isset($_GET['hotel_id']) && isset($_GET['room_id'])) {
+    
+    $userID = $_GET['user_id'];
+    $hotelID = $_GET['hotel_id'];
+    $roomID = $_GET['room_id'];
+    
+    //check if user already booked a room.
+    $query = "SELECT * FROM booking WHERE user_id={$userID} LIMIT 1";
+    $result = mysqli_query($connection, $query);
+    
+    if (!$result){
+        die ("Could not read from booking table" . mysqli_error());
+    }
+    
+    $countRows = mysqli_num_rows($result);
+    
+    if ($countRows == 0){
+        //user has not booked a room yet. insert to db
+        $query = "INSERT INTO booking (user_id, hotel_id, room_id) VALUES ($userID, $hotelID, $roomID)";
+        $result = mysqli_query($connection, $query);
+
+        if (!$result){
+            die ("could not connect to the database" . mysqli_error());
+        } 
+    } else {
+        //user has booked a room. generate error message.
+        $alreadyBookedMsg = "You have already booked a room";
+    }
+      
+}
+?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -42,7 +79,16 @@ confirm_logged_in();
         <a class="cta" href="#">Join the trip</a>
         <div class="youre-booked">
           <span class="glyphicon glyphicon-ok-circle highlighted"></span>
-          <span class="highlighted">You're already booked.</span>
+          <span class="highlighted"> 
+          <?php  
+             if (isset($alreadyBookedMsg)){
+                 echo "You're already booked.";
+             } else {
+                 echo "Please select a room";
+             }
+              
+           ?>
+          </span>
           <br>
           <a class="font-white highlighted" href="#">View my accomodation &#187;</a>
         </div>
