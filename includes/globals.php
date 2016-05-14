4ea15a902f2e96cb3d29e9d04d3f546f6dc7ad56
john wachira <john wachira>
2016-05-14 14:28:41 +0300

    Added a function to get the number of people who've booked a hotel

--- includes/globals.php
+++ includes/globals.php
@@ -28,4 +28,17 @@ function checkBookingStatus($userID){
     if ($countRows == 1)
         return true;
 }
+
+function travellersInHotel($hotelID){
+    global $connection;
+    $query = "SELECT * FROM booking WHERE hotel_id = '{$hotelID}'";
+    $result = mysqli_query($connection, $query);
+    
+    if (!$result){
+        die ("Could not read number of hotels from booking database" . mysqli_error($connection));
+    }
+    $countRows = mysqli_num_rows($result);
+    
+    return $countRows;
+}
 ?>
